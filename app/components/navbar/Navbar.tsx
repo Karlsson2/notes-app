4108f30393eb59983379506e240924ece9b1942f
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-21 11:05:21 +0200

    basic routing

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <div>
+      <Link href="/">
+        <h1>StickIT</h1>
+      </Link>
+    </div>
+  );
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,7 @@
+export default function Notes() {
+  return (
+    <main>
+      <h1>My Notes</h1>
+    </main>
+  );
+}

ce4efb8fde726b9aac6e920d93fa5c106fdff3f9
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-23 11:03:27 +0200

    fixed modal close

--- app/components/createForm/CreateForm.tsx
+++ app/components/createForm/CreateForm.tsx
@@ -1,23 +1,44 @@
 import { addNote } from "@/app/config/action";
+import React from "react";
 
-export default function CreateForm() {
+interface CreateFormProps {
+  setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function CreateForm({ setShowForm }: CreateFormProps) {
+  function handleClose() {
+    setShowForm(false);
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const { title, content, tag } = Object.fromEntries(formData);
+    addNote(formData);
+    handleClose();
+  };
+
   return (
     <div>
-        <form action={addNote}>
-            <div>
-                <label htmlFor=""></label>
-                <input type="text" name="title" placeholder="Title" />
-            </div>
-            <div>
-                <label htmlFor=""></label>
-                <input type="text" name="content" placeholder="Content" />
-            </div>
-            <div>
-                <label htmlFor=""></label>
-                <input type="text" name="tag" placeholder="Tag name" />
-            </div>
-            <button type="submit">Create Note</button>
-        </form>
+      <form onSubmit={handleSubmit} id="createForm">
+        <div>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" id="title" placeholder="Title" />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            name="content"
+            id="content"
+            placeholder="Content"
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="tag">Tag name</label>
+          <input type="text" name="tag" id="tag" placeholder="Tag name" />
+        </div>
+        <button type="submit">Create Note</button>
+      </form>
     </div>
   );
 }
--- app/components/searchbar/Searchbar.tsx
+++ app/components/searchbar/Searchbar.tsx
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 import CreateForm from "../createForm/CreateForm";
 import { useState } from "react";
 
 export default function Searchbar() {
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-    function handleClick(){
-        setShowForm(true)
-    }
+  function handleClick() {
+    setShowForm(true);
+  }
 
   return (
     <div>
-        <input type="text" placeholder="Search"/>
-        <button>Filtrering</button>
-        <button onClick={handleClick}>Add new +</button>
-        { showForm && <CreateForm></CreateForm>}
+      <input type="text" placeholder="Search" />
+      <button>Filtrering</button>
+      <button onClick={handleClick}>Add new +</button>
+      {showForm && <CreateForm setShowForm={setShowForm}></CreateForm>}
     </div>
   );
 }

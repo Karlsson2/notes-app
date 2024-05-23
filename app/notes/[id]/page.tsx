9134cd2e73d291a0e22a10c5593d949952c51170
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-23 22:09:49 +0200

    added styling

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,13 +1,13 @@
 import { GetNotes } from "@/app/config/data";
 import { updateNote } from "@/app/config/action";
 import { Note } from "@/app/components/Types/NoteTypes";
+import styles from "./note.module.css";
 
 type Params = {
   params: { id: String };
 };
 
 export default async function NotePage(params: Params) {
-
   const notes = await GetNotes();
   const slug = params.params.id;
   const note = notes.find((note: Note) => note._id === slug);
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,16 +1,19 @@
 import { GetNotes } from "../config/data";
-import Searchbar from "../components/Searchbar/Searchbar";
+import Searchbar from "../components/searchbar/Searchbar";
 import NoteComponent from "../components/Note/Note";
 import { Note } from "@/app/components/Types/NoteTypes";
+import styles from "./notes.module.css";
 
 export default async function Notes() {
   const notes: Note[] = await GetNotes();
   return (
-    <main>
+    <>
       <Searchbar />
-      {notes.map((note) => (
-        <NoteComponent key={note._id} note={note} />
-      ))}
-    </main>
+      <div className={styles.NotesContainer}>
+        {notes.map((note) => (
+          <NoteComponent key={note._id} note={note} />
+        ))}
+      </div>
+    </>
   );
 }

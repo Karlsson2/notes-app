2eaceadeacc24e821cf126fa07a2067de286e9fa
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-23 08:42:03 +0200

    added icons to notes

--- app/components/note/Note.tsx
+++ app/components/note/Note.tsx
@@ -1,5 +1,6 @@
 import { deleteNote } from "@/app/config/action";
 import Link from "next/link";
+import Image from "next/image";
 
 type NoteProps = {
   note: {
@@ -17,11 +18,18 @@ export default function Note(props: NoteProps) {
       <p>{props.note.content}</p>
       <p>{props.note.tag}</p>
       <Link href={`/notes/${props.note._id}`}>
-        <button>update</button>
+        <Image src="/icons/edit.svg" width={18} height={18} alt="edit" />
       </Link>
       <form action={deleteNote}>
         <input type="hidden" name="id" value={props.note._id} />
-        <button type="submit">delete</button>
+        <button type="submit">
+          <Image
+            src="/icons/delete.svg"
+            width={18}
+            height={18}
+            alt="edit"
+          ></Image>
+        </button>
       </form>
     </div>
   );

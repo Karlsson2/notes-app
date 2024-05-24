777f0a3c5868af77f869f52f7d6961482dd0f17a
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-24 12:07:25 +0200

    Revert "Merge pull request #8 from Karlsson2/styling"
    This reverts commit 7e2b0db9a174c1f877245721d67a4b0a55b81cee, reversing
    changes made to 96b920e80475fed8ccab5458300ebd9e8f776d31.

--- app/config/action.tsx
+++ app/config/action.tsx
@@ -3,9 +3,11 @@ import Note from "@/model/note";
 import db from "./database";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 /* import { Note } from "../components/Types/NoteTypes"; */
 /* import { redirect } from "next/navigation" */
 const addNote = async (FormData: FormData) => {
+  const router = useRouter();
   const { title, content, tag } = Object.fromEntries(FormData);
   try {
     db();
@@ -14,6 +16,12 @@ const addNote = async (FormData: FormData) => {
   } catch (error) {
     throw new Error("Failed to create note" + error);
   }
+<<<<<<< HEAD
+  revalidatePath("/");
+  redirect("/notes");
+=======
+  router.refresh();
+>>>>>>> parent of 59b4975 ( disable vercel cache)
 };
 
 const updateNote = async (FormData: FormData) => {
@@ -38,7 +46,6 @@ const deleteNote = async (FormData: FormData) => {
   try {
     db();
     await Note.findByIdAndDelete(id);
-    console.log("reeeeee");
   } catch (error) {
     throw new Error("Failed to Delete" + error);
   }

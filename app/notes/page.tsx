80d010d437321d25b0a19e034af143a02fa7926f
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-05-24 10:08:04 +0200

    fixed file ref

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,5 @@
 import { GetNotes } from "../config/data";
-import Searchbar from "../components/searchbar/Searchbar";
+import Searchbar from "../components/Searchbar/Searchbar";
 import NoteComponent from "../components/Note/Note";
 import { Note } from "@/app/components/Types/NoteTypes";
 import styles from "./notes.module.css";

2d2d5020675586af257de70c39065d1069019dc2
lucasackerberg <lucas@ackerberg.se>
2024-05-24 12:21:43 +0200

    added home page css
    added home page css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import styles from "@/app/page.module.css"
+import Image from "next/image";
+import { Kantumruy_Pro } from "next/font/google";
+
+const kantumruyPro = Kantumruy_Pro({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <Link href="/notes">
-        <h1>To My notes</h1>
+        <div className={styles.buttonBackground}>
+          <h1 className={styles.myNotes}>To My Notes</h1>
+        </div>
       </Link>
+      <Image className={styles.pictureOne} src="/kurs.svg" alt="Kurs Image" width={300} height={300} />
+      <Image className={styles.pictureTwo} src="/mote.svg" alt="Mote Image" width={300} height={300} />
+      <Image className={styles.pictureThree} src="/handlingslista.svg" alt="Handlingslista Image" width={400} height={400} />
+      <h1 className={`${styles.slogun} ${kantumruyPro.className}`}>Never forget anything with NoteIT!</h1>
     </main>
   );
 }

c7aa40e7f65c677ec30743873bcc0e03ae27a3f9
Frida Fernhede <fernhedefrida@gmail.com>
2024-05-27 11:15:04 +0200

    Removed type any from updateNote

--- app/config/action.tsx
+++ app/config/action.tsx
@@ -3,7 +3,7 @@ import Note from "@/model/note";
 import db from "./database";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-/* import { Note } from "../components/Types/NoteTypes"; */
+import { Note as NoteType } from "../components/Types/NoteTypes"; 
 /* import { redirect } from "next/navigation" */
 
 const addNote = async (FormData: FormData) => {
@@ -22,13 +22,13 @@ const addNote = async (FormData: FormData) => {
 
 const updateNote = async (FormData: FormData) => {
   const { id, title, content, tag } = Object.fromEntries(FormData);
-  id?.toString().trim().toString();
-  title?.toString().trim().toString();
-  content?.toString().trim().toString();
-  tag?.toString().trim().toString();
+  const updatedId = id?.toString().trim().toString();
+  const updatedTitle = title?.toString().trim().toString();
+  const updatedContent = content?.toString().trim().toString();
+  const updatedTag = tag?.toString().trim().toString();
   try {
     db();
-    const updateFields: any = { title, content, tag };
+    const updateFields: Partial<NoteType> = { title: updatedTitle, content: updatedContent, tag: updatedTag };
     await Note.findByIdAndUpdate(id, updateFields);
   } catch (error) {
     throw new Error("Failed to update Note" + error);
